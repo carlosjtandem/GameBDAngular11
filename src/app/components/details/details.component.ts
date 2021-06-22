1e284ba82b2e6c6dee4ce26aa1c94baa8ed0f32a
carlosjtandem <67327337+carlosjtandem@users.noreply.github.com>
2021-06-21 23:11:36 -0500

    create details of game in tabs wit angular materials

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
   gameRating = 0;
-  constructor() { }
+  gameId: string;
+  game: Game;
+  routeSub: Subscription;
+  gameSub: Subscription;
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private httpservice: HttpService
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['Id'];
+      this.gameDetails(this.gameId)
+    });
+  }
+
+  gameDetails(id: string): void {
+    this.gameSub = this.httpservice.getGameDetails(id)
+      .subscribe((gameResp: Game)=>{
+        this.game = gameResp;
+        console.log(gameResp);
+        setTimeout(() => {
+          this.gameRating= this.game.metacritic;
+        }, 1000);
+
+      });
   }
 
   getColor(value: number): string {
@@ -17,9 +45,9 @@ export class DetailsComponent implements OnInit {
       return '#5ee432';
     } else if (value > 50) {
       return 'fffa50';
-    }else if (value > 30) {
+    } else if (value > 30) {
       return 'f7aa38';
-    }else{
+    } else {
       return 'ef4655';
     }
   }
